b0eb0a029c1b7e3b35c560762dab7ed58d4cf954
Fabian Schneider <fabbbbbi+git@googlemail.com>
2016-01-30 20:28:38 +0100

    Use ES6

--- main.js
+++ main.js
@@ -1,117 +1,115 @@
-var express = require('express');
-var ical    = require('ical-generator');
-var request = require('request-promise');
+'use strict'
 
-var app = express();
+let app     = require('express')()
+let ical    = require('ical-generator')
+let request = require('request-promise')
 
 Number.prototype.pad = function(size) {
-  var s = String(this);
-  while (s.length < (size || 2)) { s = "0" + s; }
-  return s;
-};
+  let s = String(this)
+  while (s.length < (size || 2)) { s = '0' + s }
+  return s
+}
 
-app.get('/shows/:id', function (req, res) {
-  var cal = ical();
-  cal.name('TVCal');
-  cal.domain('tvcal.retromediation.net');
-  cal.prodId('//tvcal.retromediation.net//TVCal//EN');
+app.get('/shows/:id', (req, res) => {
+  let cal = ical()
+  cal.name('TVCal')
+  cal.domain('tvcal.retromediation.net')
+  cal.prodId('//tvcal.retromediation.net//TVCal//EN')
 
-  var query = req.params.id;
+  let query = req.params.id
   if (!query) {
-    return res.send('No ID given!');
+    return res.send('No ID given!')
   }
 
-  var series = query.split(';');
-
-  seriesAndEpisodes = series.map(function (s) {
-    var showName;
+  let series = query.split(';')
+  let seriesAndEpisodes = series.map( (s) => {
+    let showName
 
     return request({ url: 'http://api.tvmaze.com/shows/' + s, json: true })
-      .then(function(show) {
+      .then( (show) => {
         if (show.status !== 'Running')
-          return;
+          return
 
-        showName = show.name;
+        showName = show.name
 
-        return request({ url: 'http://api.tvmaze.com/shows/' + show.id + '/episodes', json: true });
+        return request({ url: 'http://api.tvmaze.com/shows/' + show.id + '/episodes', json: true })
       })
-      .then(function(episodes) {
+      .then( (episodes) => {
         if (!episodes)
-          return;
+          return
 
-        return episodes.filter(function (episode) {
-          return new Date(episode.airstamp) >= new Date();
-        }).forEach(function(episode) {
+        return episodes.filter( (episode) => {
+          return new Date(episode.airstamp) >= new Date()
+        }).forEach( (episode) => {
           cal.createEvent({
             start: new Date(episode.airstamp),
             end: new Date(new Date(episode.airstamp).getTime() + episode.runtime*60*1000),
             summary: showName + ' [S' + episode.season.pad() + 'E' + episode.number.pad() + ']',
             description: episode.name + '\n' + episode.url
-          });
-        });
-      });
-  });
+          })
+        })
+      })
+  })
 
-  Promise.all(seriesAndEpisodes).then(function () {
-    cal.serve(res);
-    // res.send(cal.toString());
-  });
-});
+  Promise.all(seriesAndEpisodes).then( () => {
+    cal.serve(res)
+    // res.send(cal.toString())
+  })
+})
 
-app.get('/shows/', function (req, res) {
-  var cal = ical();
-  cal.name('TVCal');
-  cal.domain('tvcal.retromediation.net');
-  cal.prodId('//tvcal.retromediation.net//TVCal//EN');
+app.get('/shows/', (req, res) => {
+  let cal = ical()
+  cal.name('TVCal')
+  cal.domain('tvcal.retromediation.net')
+  cal.prodId('//tvcal.retromediation.net//TVCal//EN')
 
-  var query = req.query.q;
+  let query = req.query.q
   if (!query) {
-    return res.send('No Query!');
+    return res.send('No Query!')
   }
 
-  var series = query.split(';');
-  console.log(series);
+  let series = query.split(';')
 
-  seriesAndEpisodes = series.map(function(s) {
-    var showName;
+  let seriesAndEpisodes = series.map((s) => {
+    let showName
 
     return request({ url: 'http://api.tvmaze.com/singlesearch/shows/?q=' + s, json: true })
-      .then(function(show) {
+      .then( (show) => {
         if (show.status !== 'Running')
-          return;
+          return
 
-        showName = show.name;
+        showName = show.name
 
-        return request({ url: 'http://api.tvmaze.com/shows/' + show.id + '/episodes', json: true });
+        return request({ url: 'http://api.tvmaze.com/shows/' + show.id + '/episodes', json: true })
       })
-      .then(function(episodes) {
+      .then( (episodes) => {
         if (!episodes)
-          return;
+          return
 
-        return episodes.filter(function(episode) {
-          return new Date(episode.airstamp) >= new Date();
-        }).forEach(function(episode) {
+        return episodes.filter( (episode) => {
+          return new Date(episode.airstamp) >= new Date()
+        }).forEach( (episode) => {
           cal.createEvent({
             start: new Date(episode.airstamp),
             end: new Date(new Date(episode.airstamp).getTime() + episode.runtime*60*1000),
             summary: showName + ' [S' + episode.season.pad() + 'E' + episode.number.pad() + ']',
             description: episode.name + '\n' + episode.url
-          });
-        });
-      });
-  });
+          })
+        })
+      })
+  })
 
-  Promise.all(seriesAndEpisodes).then(function() {
-    cal.serve(res);
-    // res.send(cal.toString());
-  });
+  Promise.all(seriesAndEpisodes).then( () => {
+    cal.serve(res)
+    // res.send(cal.toString())
+  })
 
-});
+})
 
-app.get('/*', function (req, res) {
-  res.send('');
-});
+app.get('/*', (req, res) => {
+  res.send('')
+})
 
-app.listen(5000, function () {
-  console.log('TVCal listening on port 5000!');
-});
+app.listen(5000, () => {
+  console.log('TVCal listening on port 5000!')
+})
